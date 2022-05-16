750c03cb5dbcbf506336f7ddedfb9b61e5774664
Helton Dias <helton.dias@gmail.com>
2022-05-16 15:56:22 -0300

    Swagger

--- src/main/java/br/com/pagrn/demo/DemoApplication.java
+++ src/main/java/br/com/pagrn/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package br.com.pagrn.demo;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,6 +15,16 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		//teste da Branch 2 Helton
+	}
+
+	@Bean
+	public Docket productApi(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("tads.eaj.ufrn.loja.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo( new ApiInfoBuilder().version("1.0").title("API Loja").description("Uma API para minha loja").build());
 	}
 
 }
