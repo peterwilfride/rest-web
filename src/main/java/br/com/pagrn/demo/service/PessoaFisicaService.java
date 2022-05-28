ace76f40a9cb0ffaf42c6858b420ae9c54665c0d
peterwilfride <cybermind545@gmail.com>
2022-05-28 12:32:49 -0300

    CRUD de deficiências finalizado

--- src/main/java/br/com/pagrn/demo/service/PessoaFisicaService.java
+++ src/main/java/br/com/pagrn/demo/service/PessoaFisicaService.java
@@ -1,6 +1,5 @@
 package br.com.pagrn.demo.service;
 
-import br.com.pagrn.demo.dto.PessoaFisicaDTO;
 import br.com.pagrn.demo.model.*;
 import br.com.pagrn.demo.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,48 +37,6 @@ public class PessoaFisicaService {
     public Optional<PessoaFisica> findById(Long id) {
         return pessoaFisicaRepository.findByIdAndRemoved(id, null);
     }
-
-    /*@Transactional
-    public PessoaFisicaDTO create(PessoaFisicaDTO dto) {
-
-        PessoaFisica pessoaFisica = new PessoaFisica();
-
-        Endereco endereco = enderecoRepository.findById(dto.getEndereco().getId()).orElse(null);
-
-        // fazer verificação pelos outros campos
-        if (endereco == null) {
-            endereco = new Endereco();
-        }
-
-        pessoaFisica.setNome(dto.getNome());
-        pessoaFisica.setEmail(dto.getEmail());
-        pessoaFisica.setTelefones(dto.getTelefones());
-        //pessoaFisica.setEh_pessoa_fisica(dto.getEh_pessoa_fisica());
-
-        endereco.setLatitude(dto.getEndereco().getLatitude());
-        endereco.setLongitude(dto.getEndereco().getLongitude());
-        endereco.setNumero(dto.getEndereco().getNumero());
-        endereco.setComplemento(dto.getEndereco().getComplemento());
-        endereco.setLogradouro(dto.getEndereco().getLogradouro());
-        endereco.setCep(dto.getEndereco().getCep());
-        pessoaFisica.setEndereco(endereco);
-
-        pessoaFisica.setSexo(dto.getSexo());
-        pessoaFisica.setCpf(dto.getCpf());
-        pessoaFisica.setTipo_sangue(dto.getTipo_sangue());
-        pessoaFisica.setData_nascimento(dto.getData_nascimento());
-        pessoaFisica.setNome_pai(dto.getNome_pai());
-        pessoaFisica.setNome_mae(dto.getNome_mae());
-        pessoaFisica.setFoto(dto.getFoto());
-
-        pessoaFisica.setServidores(dto.getServidores());
-
-        pessoaFisica.setDeficiencias(dto.getDeficiencias());
-
-        pessoaFisica = pessoaFisicaRepository.save(pessoaFisica);
-
-        return new PessoaFisicaDTO(pessoaFisica);
-    }*/
 
     @Transactional
     public PessoaFisica create(PessoaFisica pessoaFisica) {
--- src/main/java/br/com/pagrn/demo/service/PessoaJuridicaService.java
+++ src/main/java/br/com/pagrn/demo/service/PessoaJuridicaService.java
@@ -1,21 +1,16 @@
 package br.com.pagrn.demo.service;
 
-import br.com.pagrn.demo.dto.PessoaJuridicaDTO;
 import br.com.pagrn.demo.model.Endereco;
-import br.com.pagrn.demo.model.PessoaFisica;
 import br.com.pagrn.demo.model.PessoaJuridica;
 import br.com.pagrn.demo.repository.EnderecoRepository;
 import br.com.pagrn.demo.repository.PessoaJuridicaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -31,61 +26,9 @@ public class PessoaJuridicaService {
         return pessoaJuridicaRepository.findAllByRemoved(null, pegeable);
     }
 
-    /*
-    public Page<PessoaJuridica> find() {
-        Para cada pessoa jurifica no banco de dados, verificar
-        * se o atributo removed é null, se sim, adiciona a pessoa
-        * na lista, se não ignora. Ao final converte a lista para
-        * um Page.
-        *
-        List<PessoaJuridica> pjs = pessoaJuridicaRepository.findAll();
-        List<PessoaJuridica> new_pjs = new ArrayList<>();
-        for(int i = 0; i < pessoaJuridicaRepository.count(); i++) {
-            PessoaJuridica pj = pjs.get(i);
-            if (pj.getRemoved() == null) {
-                new_pjs.add(pj);
-            }
-        }
-        return new PageImpl<>(new_pjs);
-    }*/
-
     public Optional<PessoaJuridica> findById(Long id) {
         return pessoaJuridicaRepository.findByIdAndRemoved(id,null);
     }
-
-    /*@Transactional
-    public PessoaJuridicaDTO create(PessoaJuridicaDTO dto) {
-
-        PessoaJuridica pessoaJuridica = new PessoaJuridica();
-
-        Endereco endereco = enderecoRepository.findById(dto.getEndereco().getId()).orElse(null);
-
-        // fazer verificação pelos outros campos
-        if (endereco == null) {
-            endereco = new Endereco();
-        }
-
-        pessoaJuridica.setNome(dto.getNome());
-        pessoaJuridica.setEmail(dto.getEmail());
-        pessoaJuridica.setTelefones(dto.getTelefones());
-        //pessoaJuridica.setEh_pessoa_fisica(dto.getEh_pessoa_fisica());
-        pessoaJuridica.setEndereco(dto.getEndereco());
-
-        endereco.setLatitude(dto.getEndereco().getLatitude());
-        endereco.setLongitude(dto.getEndereco().getLongitude());
-        endereco.setNumero(dto.getEndereco().getNumero());
-        endereco.setComplemento(dto.getEndereco().getComplemento());
-        endereco.setLogradouro(dto.getEndereco().getLogradouro());
-        endereco.setCep(dto.getEndereco().getCep());
-        pessoaJuridica.setEndereco(endereco);
-
-        pessoaJuridica.setCnpj(dto.getCnpj());
-        pessoaJuridica.setRazao_social(dto.getRazao_social());
-
-        pessoaJuridica = pessoaJuridicaRepository.save(pessoaJuridica);
-
-        return new PessoaJuridicaDTO(pessoaJuridica);
-    }*/
 
     @Transactional
     public PessoaJuridica create(PessoaJuridica pessoaJuridica) {
